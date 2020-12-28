2bfd0f29cd482c14d565f00db1493d87c24be6a6
h.salari <hadi.salari2000@gmail.com>
2020-12-28 12:40:27 +0330

    Initial commit

---
+++ ms-uaa/src/main/java/com/salari/framework/uaa/MsUaaApplication.java
@@ -0,0 +1,13 @@
+package com.salari.framework.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsUaaApplication.class, args);
+    }
+
+}
